84914662bd9dafeb9c6dff2498f82c8811790ffb
NavikkhaArvind <navikkhaarvind6@gmail.com>
2026-07-14 22:05:21 +0530

    Create 07_Final_Report.sql

---
+++ SQL/07_Final_Report.sql
@@ -0,0 +1,17 @@
+/*
+FINAL BUSINESS INSIGHTS
+
+1. Month-to-month contracts have the highest churn.
+2. Customers with lower satisfaction scores are more likely to churn.
+3. Customers with higher total charges contribute most to revenue.
+4. State X has the highest number of churned customers.
+5. Offer acceptance appears to reduce churn.
+
+BUSINESS RECOMMENDATIONS
+
+1. Encourage long-term contracts.
+2. Improve customer support.
+3. Provide loyalty rewards.
+4. Target high-value customers with retention offers.
+5. Follow up with customers who report multiple complaints.
+*/
